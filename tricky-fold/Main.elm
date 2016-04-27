b36d5349ee1bfd1bb488ac0b46cd822544bda320
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-04-27 15:31:21 +0100

    Replace String by actual Date.Day for days

--- tricky-fold/Main.elm
+++ tricky-fold/Main.elm
@@ -1,27 +1,28 @@
 import Html exposing (text)
+import Date exposing (Day(Mon, Tue, Wed))
 
 type alias ForecastItem =
-  { day: String
+  { day: Day
   , hour: Int
   }
 
 type alias ForecastsPerDay =
-  (String, List ForecastItem)
+  (Day, List ForecastItem)
 
 input : List ForecastItem
 input =
-  [ {day = "Mon", hour = 6}
-  , {day = "Mon", hour = 9}
-  , {day = "Tue", hour = 6}
-  , {day = "Tue", hour = 9}
-  , {day = "Wed", hour = 6}
+  [ {day = Mon, hour = 6}
+  , {day = Mon, hour = 9}
+  , {day = Tue, hour = 6}
+  , {day = Tue, hour = 9}
+  , {day = Wed, hour = 6}
   ]
 
 output : List ForecastsPerDay
 output =
-  [ ("Mon", [{day = "Mon", hour = 6}, {day = "Mon", hour = 9}])
-  , ("Tue", [{day = "Tue", hour = 6}, {day = "Tue", hour = 9}])
-  , ("Wed", [{day = "Wed", hour = 6}])
+  [ (Mon, [{day = Mon, hour = 6}, {day = Mon, hour = 9}])
+  , (Tue, [{day = Tue, hour = 6}, {day = Tue, hour = 9}])
+  , (Wed, [{day = Wed, hour = 6}])
   ]
 
 groupByDay : ForecastItem -> List ForecastsPerDay -> List ForecastsPerDay
@@ -31,6 +32,8 @@ groupByDay x acc =
     default = (day, [x]) :: acc
   in
     case acc of
+      [] ->
+        default
       h::_ ->
         let
           (hDay, hRef) = h
@@ -40,8 +43,6 @@ groupByDay x acc =
               (day, x :: hRef) :: accTail
           else
             default
-      [] ->
-        default
 
 result =
   List.foldr groupByDay [] input
